2f23b7e21b3d1d6776f37ea6d033ee011e131627
hd-c <bohemiantroye@gmail.com>
2022-11-11 19:27:21 +0200

    task3

--- code/index.php
+++ code/index.php
@@ -19,3 +19,11 @@ $last_month = 1187.23;
 $this_month = 1089.98;
 echo "the difference in spending money = " . ($last_month - $this_month) . "\n";
 // endregion
+
+//region task3: Division and multiplication
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
+echo $days_per_language . "\n";
+// endregion

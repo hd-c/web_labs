85f7b0ab8f618b8db01e7e147ab0cd4a837f68ac
hd-c <bohemiantroye@gmail.com>
2022-11-11 19:10:48 +0200

    task1

--- code/index.php
+++ code/index.php
@@ -1,2 +1,7 @@
 <?php
-echo phpinfo();
+// region task1: Link access
+$very_bad_unclear_name = "15 chicken wings";
+$order = $very_bad_unclear_name;
+$order .= "and a cup of milk for my cat";
+echo $very_bad_unclear_name . "\n";
+// endregion
